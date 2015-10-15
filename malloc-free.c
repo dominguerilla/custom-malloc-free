de8537c6b64463fbf6243f9b5ac38fa2760ee0c3
dominguerilla <dominguerilla@gmail.com>
2015-10-15 19:42:02 +0000

    reorganized a bit, updated malloc

--- malloc-free.c
+++ malloc-free.c
@@ -1,47 +1,93 @@
 #include "malloc-free.h"
 
+static char memory[MAX_SIZE] = {'x'};
+static char is_alloc[MAX_SIZE] = {'0'};
+static int size_of_chunk[MAX_SIZE] = {0};
+
 static unsigned int front_index;
 static unsigned int rear_index = MAX_SIZE;
-
-static char memory[MAX_SIZE] = {'x'};
-/*static char is_alloc[MAX_SIZE] = {'0'};*/
 static unsigned int bytes_used;
+
+void* allocate(int size, char IS_LARGE){
+	if(IS_LARGE == 'n'){
+		/*
+		* First, update the tracking values--bytes_used, size_of_chunk,
+		* and is_alloc.
+		*
+		*/
+		bytes_used = bytes_used + size;
+		size_of_chunk[front_index] = size;
+		int i;
+		for(i = front_index; i < size; i++){
+			is_alloc[i] = '1';
+		}
+		
+		/*
+		* Now that the proper tracking values are updated, the position of
+		* 'frontier' between the allocated space and 'free space' is updated
+		* and its position returned.
+		*
+		*/
+		int start_of_chunk = front_index;
+		front_index = front_index + size;
+		return &memory[start_of_chunk];
+	}else{
+		/*
+		* First, update the tracking values--bytes_used, size_of_chunk,
+		* and is_alloc.
+		*
+		*/
+		bytes_used = bytes_used + size;
+		size_of_chunk[rear_index - size] = size;
+		int i;
+		for(i = rear_index; i < size; i++){
+			is_alloc[rear_index] = '1';
+			rear_index--;
+		}
+
+		/*
+		* Now that the proper tracking values are updated, the position of
+		* 'frontier' between the allocated space and 'free space' is updated
+		* and its position returned.
+		*
+		*/
+		return &memory[rear_index];
+	}
+}
 
 void* my_malloc(size_t t){
 	printf("size_t: %zd\n", t);
+	/* If there is no more memory...*/
 	if(bytes_used >= MAX_SIZE){
 		printf("Out of memory!\n");
 		return NULL;
 	}
+	
+	/* If the used memory + the size of the attempted allocation is greater
+	*  than maximum...
+	*/
 	if(bytes_used + t > MAX_SIZE){
 		printf("Not enough memory for this allocation.\n");
 		return NULL;
 	}
+
+	/* If the amount of memory to be allocated is considered 'small'*/
 	if(t < LARGE_CHUNK_SIZE){
 		if(front_index + t + 1 >= rear_index){
 			printf("Not enough memory for this allocation.\n");
 			return NULL;
 		}
-		bytes_used = bytes_used + t + 1;
-		memory[front_index] = t;
-		printf("memory[front_index] = %c\n", memory[front_index]);
-		int position = front_index + 1;
-		front_index = position + t;
-		return &memory[position];
+		return allocate((int)t,'n');
 	}
+
+	/* If the amount of memory to be allocated is considered 'large'*/
 	else{
 		if(MAX_SIZE - rear_index - t - 1 <= front_index){
 			printf("Not enough memory for this allocation.\n");
 			return NULL;
 		}
-		bytes_used = bytes_used + t + 1;
-		rear_index = rear_index - t - 1;
-		memory[rear_index] = t;
-		return &memory[rear_index];
+		return allocate((int)t,'y');
 	}
-
-	printf("you shouldn't have gotten here\n");
-	return NULL;
 }
 
 void my_free(void* ptr){
@@ -53,6 +99,8 @@ void my_free(void* ptr){
 		printf("You did not malloc this pointer.\n");
 		return;
 	}
+
+
 	void* prev = ptr-1;
 	int size = *((int*)prev);
 	printf("the size of this pointer is %d\n", size);
